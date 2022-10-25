55cad60810fe1d1af3a2d5f6d1dc0aacea63f9b9
khadijaa34 <115382212+khadijaa34@users.noreply.github.com>
2022-10-25 17:14:34 +0200

    Add files via upload
    I have made some changes based on the review. the class names are not the same as written in the review it would be a problem for me to change all those classes. please check the classes and you will find that it contains the things you want. Thank you.

---
+++ src/invoiceapp/InvoiceApp/src/Model/Items.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Dejaa
+ */
+public class Items {
+    int id;
+    String ItemName;
+    String price;
+    String count;
+
+    public Items(int id, String ItemName, String price, String count) {
+        this.id = id;
+        this.ItemName = ItemName;
+        this.price = price;
+        this.count = count;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String ItemName) {
+        this.ItemName = ItemName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" + "id=" + id + ", ItemName=" + ItemName + ", price=" + price + ", count=" + count + '}';
+    }
+
+    
+}
