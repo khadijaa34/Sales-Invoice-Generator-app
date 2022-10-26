298eb2cda36d4495be0e380888830cf61eb6fbf3
khadijaa34 <115382212+khadijaa34@users.noreply.github.com>
2022-10-26 22:35:56 +0200

    Add files via upload

---
+++ src/invoiceapp/Project/InvoiceApp/src/Model/Invoice.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Model.Items;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Dejaa
+ */
+public class Invoice {
+    
+    int id;
+    String date;
+    String CustomerName;
+    String total;
+    ArrayList <Items> items; 
+
+    public Invoice(int id, String CustomerName, String date) {
+        this.id = id;
+        this.CustomerName = CustomerName;
+        this.date = date;
+        this.items = new ArrayList<Items>();
+
+    }
+
+    public Invoice(int id, String date, String CustomerName, String total) {
+        this.id = id;
+        this.date = date;
+        this.CustomerName = CustomerName;
+        this.total = total;
+    }
+    
+    
+
+    Invoice() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    public void setCustomerName(String CustomerName) {
+        this.CustomerName = CustomerName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public ArrayList<Items> getItems() {
+        return items;
+    }
+    
+    
+    
+}
---
+++ src/invoiceapp/Project/InvoiceApp/src/Model/InvoiceApp.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import Model.Items;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Dejaa
+ */
+public class InvoiceApp {
+    /**
+     * @param args the command line arguments
+     */
+    
+    static Invoice i;
+   
+    
+    // method for loading the data from the file into the table //
+    public static ArrayList<Invoice> LoadData() throws FileNotFoundException, IOException{
+        Items item;
+        int total;
+        int subTotal;
+        ArrayList<Invoice> list = new ArrayList<Invoice>();
+        // 1st file //
+        File fHeader = new File("C:\\Users\\Dejaa\\Desktop\\Sales Invoice Generator\\InvoiceHeader.csv");
+        BufferedReader br = new BufferedReader(new FileReader(fHeader));
+        
+        //2 nd file //
+        File fLine = new File("C:\\Users\\Dejaa\\Desktop\\Sales Invoice Generator\\InvoiceLine.csv");
+        BufferedReader brItem = new BufferedReader(new FileReader(fLine));
+        
+        String st,stItem;
+        while ((st = br.readLine()) != null) {          
+        
+            String[] inv = st.split(","); 
+            if(inv != null && inv.length>0){
+            i = new Invoice (Integer.parseInt(inv[0]),inv[2], inv[1]);
+            total = 0;
+            
+            //Read Invoice line file
+            brItem = new BufferedReader(new FileReader(fLine));
+            while ((stItem = brItem.readLine()) != null) { 
+                String[] it = stItem.split(",");
+                if(it != null && it.length>0){
+                    if(Integer.parseInt(it[0]) == i.id)
+                    {
+                     item  = new Items(Integer.parseInt(it[0]),it[1],it[2],it[3]);
+                     subTotal = Integer.parseInt(it[2]) * Integer.parseInt(it[3]);
+                     total += subTotal;   
+                     i.items.add(item);
+                    }
+                }          
+            }
+            i.total = Integer.toString(total);
+            list.add(i);    
+            }        
+    }    
+    return list;
+}
+    /////////////////////
+    
+
+    public static void SaveData(ArrayList<Invoice> data, String filePath){
+        try{
+           FileWriter fw = new FileWriter(filePath);
+                      
+           if (data != null && data.size() > 0)
+           {
+               for(int i=0; i<data.size(); i++){
+                   String line = Integer.toString(data.get(i).getId()) + "," + 
+                    data.get(i).getDate() + "," + data.get(i).getCustomerName() 
+                    + String.format("%n");
+                   fw.write(line );                   
+               }
+           }
+           fw.close();
+          }catch(Exception e){
+          System.out.println(e);
+          }
+    }
+    
+//    public static void ItemsData(ArrayList<Items> dataItems, String itemsFilePath){
+//        
+//         try{
+//           FileWriter fw = new FileWriter(itemsFilePath);
+//                      
+//           if (dataItems != null && dataItems.size() > 0)
+//           {
+//               for(int i=0; i<dataItems.size(); i++){
+//                   String line = Integer.toString(dataItems.get(i).getId()) + "," + 
+//                           dataItems.get(i).getItemName() + "," + dataItems.get(i).getPrice() + "," 
+//                           + dataItems.get(i).getCount() + "," + dataItems.get(i).getTotal() 
+//                           + String.format("%n");
+//                   fw.write(line );                   
+//               }
+//           }
+//           fw.close();
+//          }catch(Exception e){
+//          System.out.println(e);
+//          }
+//        
+// 
+//    }
+    
+    
+    /////////////////////
+ 
+    
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+
+        
+    }
+    
+}
+
